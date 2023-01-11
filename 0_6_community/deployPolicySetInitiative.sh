3c6563c2907e54b55d8b9ee63d4f6c3e8918a414
Hisashi Goto <higoto@microsoft.com>
2023-01-11 13:19:11 +0900

    debug

--- 0_6_community/deployPolicySetInitiative.sh
+++ 0_6_community/deployPolicySetInitiative.sh
@@ -3,7 +3,7 @@
 for  filename in `ls ./initiatives/*/policyset.json -1`; do
     export PolicySetName="`cat  $filename | jq -r '.name'`"
     export SubscriptionId="`az account show | jq -r '.id'`"
-    export PolicySetDescription="`cat $filename | jq -r '.properties.description'`"
+    export PolicySetDescription="\"`cat $filename | jq -r '.properties.description'`\""
     export PolicySetName="`cat $filename | jq -r '.name'`"
     export PolicySetDisplayName="\"`cat $filename | jq -r '.properties.displayName'`\""
     ## replace  __your_subscription_id__  to $SubscriptionId ##
