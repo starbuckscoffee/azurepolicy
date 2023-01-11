4fb5fbc6c86d6a737fdb5e594cc8903d75d90606
Hisashi Goto <higoto@microsoft.com>
2023-01-11 16:39:15 +0900

    debug

--- 0_6_community/deployPolicySetInitiative.sh
+++ 0_6_community/deployPolicySetInitiative.sh
@@ -8,6 +8,6 @@ for  filename in `ls ./initiatives/*/policyset.json -1`; do
     export PolicySetDisplayName="`cat $filename | jq -r '.properties.displayName'`"
     ## replace  __your_subscription_id__  to $SubscriptionId ##
     sed s/__your_subscription_id__/${SubscriptionId}/g  DIRNAME/policyDefinisionSet.template > DIRNAME/policyDefinisionSet.json
-    az policy set-definition create --definitions DIRNAME/policyDefinisionSet.json --name $PolicySetName --description  "$PolicySetDescription" --display-name "$PolicySetDisplayName" --metadata category=PrivateDnsZoneGroupId --params .DIRNAME/policyDefinisionParams.json
+    az policy set-definition create --definitions $(DIRNAME/policyDefinisionSet.json) --name $PolicySetName --description  "$PolicySetDescription" --display-name "$PolicySetDisplayName" --metadata category=PrivateDnsZoneGroupId --params $(DIRNAME/policyDefinisionParams.json)
 
 done
