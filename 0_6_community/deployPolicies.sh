9a9634486fbfa9854cd0ebd664d3f628f2821a18
Hisashi Goto <higoto@microsoft.com>
2023-01-11 11:45:12 +0900

    Debug

--- 0_6_community/deployPolicies.sh
+++ 0_6_community/deployPolicies.sh
@@ -2,8 +2,11 @@
 
 for  filename in `ls ./policies/*/policy.json -1`; do
     export PolicyCategory=`cat  $filename | jq -r '.properties.metadata.category'`
+    echo $PolicyCategory
     export PolicyDisplayName="`cat $filename | jq -r '.properties.displayName'`"
+    echo $PolicyDisplayName
     export PolicyName="`cat  $filename | jq -r '.name'`"
+    echo $PolicyName
     az policy definition create --name $PolicyName --display-name $PolicyDisplayName --mode Indexed --rules ${filename%.*}.rule --params ${filename%.*}.params --subscription 
 done
 
