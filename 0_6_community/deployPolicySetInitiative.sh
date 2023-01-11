2d917fa2689178599a65ffbe29ed75bc049d7e26
Hisashi Goto <higoto@microsoft.com>
2023-01-11 10:54:54 +0900

    Add  2 bash scripts for deploy azure policies

---
+++ 0_6_community/deployPolicySetInitiative.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for  filename in `ls ./initiatives/*/policyset.json -1`; do
+    export PolicySetName="`cat  $filename | jq -r '.name'`"
+    export SubscriptionId="`az account show | jq -r '.id'`"
+    export PolicySetDescription="`cat $filename | jq -r '.properties.description'`"
+    export PolicySetName="`cat $filename | jq -r '.name'`"
+    export PolicySetDisplayName="`cat $filename | jq -r '.properties.displayName'`"
+    ## replace  __your_subscription_id__  to $SubscriptionId ##
+    sed s/__your_subscription_id__/${SubscriptionId}/g  DIRNAME/policyDefinisionSet.template > DIRNAME/policyDefinisionSet.json
+    az policy set-definition create --definitions DIRNAME/policyDefinisionSet.json --name $PolicySetName --description  $PolicySetDescription --display-name $PolicySetDisplayName --metadata category=PrivateDnsZoneGroupId --params .DIRNAME/policyDefinisionParams.json
+
+done
